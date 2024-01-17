a03589f247fd27ebbd4b64966f4302e70229664a
sem7655 <127785162+sem7655@users.noreply.github.com>
2024-01-18 00:23:54 +0600

    Update settings.py

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -78,11 +78,17 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators

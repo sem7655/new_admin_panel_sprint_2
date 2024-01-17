1381470d760293133ec84e16466e4bdf9920dfcc
sem7655 <127785162+sem7655@users.noreply.github.com>
2024-01-18 00:33:17 +0600

    Update run_uwsgi.sh

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -3,5 +3,7 @@
 set -e
 
 chown www-data:www-data /var/log
-
+python manage.py makemessages -l en -l ru
+python manage.py collectstatic --noinput
+python manage.py migrate --fake-initial
 uwsgi --strict --ini /etc/app/uwsgi.ini

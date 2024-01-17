5e0d881b3ad4f1739625637af5804a0249122ec8
sem7655 <127785162+sem7655@users.noreply.github.com>
2024-01-18 00:21:48 +0600

    Update admin.py

--- docker_compose/simple_project/app/movies/admin.py
+++ docker_compose/simple_project/app/movies/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Genre
+from .models import Filmwork
+from .models import Person
+from .models import PersonFilmwork
+from .models import GenreFilmwork
+
+
+# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created', 'modified')
+    search_fields = ('name', 'description', 'id')
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
+    list_filter = ('type', 'genres', 'rating',)
+    search_fields = ('title', 'description', 'id')
+
+@admin.register(Person)
+class Person(admin.ModelAdmin):
+    list_display = ('full_name', 'created', 'modified')
+    search_fields = ('full_name', 'id')

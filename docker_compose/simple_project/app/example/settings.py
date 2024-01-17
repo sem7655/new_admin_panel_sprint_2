a9531cb15cc5da347b4208a499ea3630e37d7f4b
sem7655 <127785162+sem7655@users.noreply.github.com>
2024-01-18 00:16:43 +0600

    Update settings.py

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from split_settings.tools import include
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
